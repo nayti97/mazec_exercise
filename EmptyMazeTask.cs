431d048c9527be6a9ca57fed1ee9e038e81f4bd8
nayti <nayti1997@gmail.com>
2017-11-16 19:25:26 +0500

    git init

---
+++ EmptyMazeTask.cs
@@ -0,0 +1,15 @@
+﻿namespace Mazes
+{
+	public static class EmptyMazeTask
+	{
+		public static void MoveOut(Robot robot, int width, int height)
+		{
+			while (!robot.Finished) {
+				if (robot.Y < width-2)
+					robot.MoveTo (Direction.Right);
+				if (robot.X < height-2)
+					robot.MoveTo (Direction.Down);
+			}
+		}
+	}
+}
---
+++ SnakeMazeTask.cs
@@ -0,0 +1,39 @@
+﻿namespace Mazes
+{
+	public static class SnakeMazeTask
+	{
+		public static void MoveOut(Robot robot, int width, int height)
+		{
+			string side = "right";
+			while (!robot.Finished) {
+				if (robot.X == 1 && robot.Y != 1) {
+					side = "right"; 
+					PassDown (robot);
+					robot.MoveTo (Direction.Right);
+				} else if (robot.X == width - 2) {
+					side = "left"; 
+					PassDown (robot);
+					robot.MoveTo (Direction.Left);
+				}
+
+				MoveInSide (robot, side);
+			}
+		}
+
+		public static void PassDown(Robot robot)
+		{
+			for (var i = 0; i < 2; i++) {
+				robot.MoveTo (Direction.Down);
+			}
+		}
+
+		public static void MoveInSide(Robot robot, string side)
+		{
+			if (side == "left") {
+				robot.MoveTo (Direction.Left);
+			} else {
+				robot.MoveTo (Direction.Right);
+			}
+		}
+	}
+}
